d7f6d9c708bd48e0406517a21be96e3f949dceb2
swonny <wonny921@gmail.com>
2022-12-08 14:43:50 +0900

    feat(Station, StationTest): 지하철 역 이름 유효성 검사 추가, 테스트 코드 추가

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -6,10 +6,11 @@ public class Station {
     * 필드(인스턴스 변수)인 name의 접근 제어자 private을 변경할 수 없다.
     * 가능하면 setter 메소드(ex. setXXX)를 추가하지 않고 구현한다.
     * */
-
+    private final int MINIMUM_NAME_LENGTH = 2;
     private String name;
 
     public Station(String name) {
+        validate(name);
         this.name = name;
     }
 
@@ -17,5 +18,9 @@ public class Station {
         return name;
     }
 
-    // 추가 기능 구현
+    private void validate(String name) {
+        if (name.length() < MINIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
--- src/test/java/subway/domain/StationTest.java
+++ src/test/java/subway/domain/StationTest.java
@@ -3,6 +3,7 @@ package subway.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StationTest {
@@ -10,5 +11,11 @@ class StationTest {
     public void testStation() {
         Station station = new Station("송내역");
         assertThat(station.getName()).isEqualTo("송내역");
+    }
+
+    @Test
+    public void testValidate() {
+        assertThatThrownBy(() -> new Station("송"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
